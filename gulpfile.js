431f36a23a48e1e6fbe28af644c2b3e333894423
Bogdan Dimofte <bogdan.dimofte+github@shop.co>
2018-03-04 12:38:41 +0200

    gulp: task `server` only deals with the node app (hot reload)

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,61 @@
 'use strict';
-var gulp = require('gulp');
-var livereload = require('gulp-livereload');
-var server = require( 'gulp-develop-server' );
+const gulp = require('gulp');
+const livereload = require('gulp-livereload');
+const server = require('gulp-develop-server');
 
-var serverFiles = [
-    './app.js'
-];
+const serverFiles = ['./src/**/*'];
 
-
-gulp.task('default', ['server:start', 'js:watch', 'css:watch', 'html:watch'], function(){
-  function restart( file ) {
-      server.changed( function( error ) {
-          if( ! error ) livereload.changed( file.path );
-      });
-  }
-  gulp.watch( serverFiles ).on( 'change', restart );
-
-});
-gulp.task('server', ['default']);
-
-gulp.task( 'server:start', function() {
-    server.listen( { path: './app.js' }, function(err){
-        if(err){return;}
-        livereload.listen;
+gulp.task('default', ['server:start', 'js:watch', 'css:watch', 'html:watch'], function() {
+  function restart(file) {
+    server.changed(function(error) {
+      if (!error) livereload.changed(file.path);
     });
+  }
+  gulp.watch(serverFiles).on('change', restart);
+});
+
+
+gulp.task('server', ['server:start'], function() {
+  function restart(file) {
+    server.changed(function(error) {
+      if (!error) livereload.changed(file.path);
+    });
+  }
+  gulp.watch(serverFiles).on('change', restart);
+});
+
+gulp.task('server:start', function() {
+  server.listen({ path: './src/app.js' }, function(err) {
+    if (err) {
+      return;
+    }
+    livereload.listen;
+  });
 });
 
 gulp.task('html', function() {
-  gulp.src('./public/*.html')
-    .pipe(livereload());
+  gulp.src('./public/*.html').pipe(livereload());
 });
 
-gulp.task('html:watch', function () {
+gulp.task('html:watch', function() {
   livereload.listen();
   gulp.watch('./public/*.html', ['html']);
 });
 
 gulp.task('css', function() {
-  gulp.src('./public/css/**/*.css')
-    .pipe(livereload());
+  gulp.src('./public/css/**/*.css').pipe(livereload());
 });
 
-gulp.task('css:watch', function () {
+gulp.task('css:watch', function() {
   livereload.listen();
   gulp.watch('./public/css/**/*.css', ['css']);
 });
 
 gulp.task('js', function() {
-  gulp.src('./public/js/**/*.js')
-    .pipe(livereload());
+  gulp.src('./public/js/**/*.js').pipe(livereload());
 });
 
-gulp.task('js:watch', function () {
+gulp.task('js:watch', function() {
   livereload.listen();
   gulp.watch('./public/js/**/*.js', ['js']);
 });
