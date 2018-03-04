6a04c34779270aa9a30546fd53646044a4b502af
Bogdan Dimofte <bogdan.dimofte+github@shop.co>
2018-03-04 21:15:05 +0200

    tests: return printed results (correct use-case), various error cases

--- tests/rest.spec.js
+++ tests/rest.spec.js
@@ -8,10 +8,9 @@ const { server, stop } = require('../src/main');
 const { expect } = chai;
 
 chai.use(chaiHttp);
-const path = '/js';
+const path = '/python';
 
-const isCompilationError = text => text.substr(0, 17) === 'CompilationError:';
-const isExecutionError = text => text.substr(0, 15) === 'ExecutionError:';
+const isSyntaxError = msg => msg.includes('SyntaxError: invalid syntax');
 
 describe('POST js', () => {
   // beforeEach(done => done());
@@ -19,47 +18,41 @@ describe('POST js', () => {
   before(async () => {
     chaiServer = await chai.request(server);
   });
-  after(async() => stop());
+  after(async () => stop());
 
   it('it should error for requests without body', done => {
-    chaiServer
-      .post(path)
-      .end((err, res) => {
-        expect(err).not.to.be.null;
-        expect(res).to.have.status(400);
-        expect(isCompilationError(res.text)).to.be.true;
-        done();
-      });
+    chaiServer.post(path).end((err, res) => {
+      expect(err).not.to.be.null;
+      expect(res).to.have.status(400);
+      expect(isSyntaxError(res.text)).to.be.true;
+      done();
+    });
   });
 
-  it('it should return the correct last value', done => {
-    // plain number
-    let payload = 'const x = 1; x + 1'; // '2'
+  it('it should return anything that was printed', done => {
+    // identation is important in python!
+    const payload = `
+x = 1
+while x < 5:
+    print(x)
+    x = x + 1
+      `;
     chaiServer
       .post(path)
       .set('content-type', 'text/plain')
       .send(payload)
       .end((err, res) => {
+        expect(err).to.be.null;
         expect(res).to.have.status(200);
-        expect(JSON.parse(res.text)).to.equal(2);
-      });
-
-    // a more complex object
-    payload = 'const x = {foo: 1, bar: 2}; x';
-    chaiServer
-      .post(path)
-      .set('content-type', 'text/plain')
-      .send(payload)
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(JSON.parse(res.text)).to.deep.equal({foo: 1, bar: 2});
+        expect(JSON.parse(res.text)).to.deep.equal(['1', '2', '3', '4']);
         done();
       });
   });
 
-  it('it should report execution errors', done => {
+  it('it should report "not defined" errors', done => {
     // ExecutionError:  foo is not defined
     const payload = 'foo()';
+    const errorMessage = 'NameError: name \'foo\' is not defined';
     chaiServer
       .post(path)
       .set('content-type', 'text/plain')
@@ -67,13 +60,14 @@ describe('POST js', () => {
       .end((err, res) => {
         expect(err).not.to.be.null;
         expect(res).to.have.status(400);
-        expect(isExecutionError(res.text)).to.be.true;
+        expect(res.text.includes(errorMessage)).to.be.true;
         done();
       });
   });
 
-  it('it should report compilation errors', done => {
-    const payload = 'const x = 2; *#@$$';
+  it('it should report syntax errors', done => {
+    const payload = '^$%^';
+    const errorMessage =  'SyntaxError: invalid syntax';
     chaiServer
       .post(path)
       .set('content-type', 'text/plain')
@@ -81,51 +75,32 @@ describe('POST js', () => {
       .end((err, res) => {
         expect(err).not.to.be.null;
         expect(res).to.have.status(400);
-        expect(isCompilationError(res.text)).to.be.true;
+        // console.log(res.text );
+        expect(res.text.includes(errorMessage)).to.be.true;
         done();
       });
   });
 
+  // TODO: timebox script, cut access to filesystem etc
 
-  it('it should reject returning object literals', done => {
-    const payload = '{ foo: 101, bar: 120 }';
-    chaiServer
-      .post(path)
-      .set('content-type', 'text/plain')
-      .send(payload)
-      .end((err, res) => {
-        expect(err).not.to.be.null;
-        expect(res).to.have.status(400);
-        expect(isCompilationError(res.text)).to.be.true;
-        done();
-      });
-  });
-
-  it('it should error the execution of long scripts (limit: 1.5s)', done => {
-    const payload = 'while(true) {}';
-    chaiServer
-      .post(path)
-      .set('content-type', 'text/plain')
-      .send(payload)
-      .end((err, res) => {
-        expect(err).not.to.be.null;
-        expect(res).to.have.status(400);
-        expect(isExecutionError(res.text)).to.be.true;
-        done();
-      });
-  });
-
-  it('it should not provide access to node modules', done => {
-    const payload = 'const fs = require(\'fs\')'; // fs would be quite dangerous :)
-    chaiServer
-      .post(path)
-      .set('content-type', 'text/plain')
-      .send(payload)
-      .end((err, res) => {
-        expect(err).not.to.be.null;
-        expect(res).to.have.status(400);
-        expect(isExecutionError(res.text)).to.be.true;
-        done();
-      });
-  });
+//   it('it should not provide write access to the filesystem', done => {
+//     const payload = `
+// file = open("testfile.txt", "w")
+// file.write("This is a test")
+// file.close()
+// file = open("testfile.txt", "r")
+// print(file.read())
+//     `;
+//     chaiServer
+//       .post(path)
+//       .set('content-type', 'text/plain')
+//       .send(payload)
+//       .end((err, res) => {
+//         expect(err).not.to.be.null;
+//         expect(res).to.have.status(400);
+//         console.log(res.text );
+//         // expect(res.text.includes(errorMessage)).to.be.true;
+//         done();
+//       });
+//   });
 });
