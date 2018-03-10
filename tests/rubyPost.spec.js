d47d7fd62b68d39e0afe0f3a393526dd7fc43633
Bogdan Dimofte <bogdan.dimofte+github@shop.co>
2018-03-10 13:55:44 +0200

    tests: for POST `/ruby`

--- tests/rubyPost.spec.js
+++ tests/rubyPost.spec.js
@@ -8,11 +8,9 @@ const { startServer, stopServer } = require('../src/express');
 const { expect } = chai;
 
 chai.use(chaiHttp);
-const path = '/python';
+const path = '/ruby';
 
-const isSyntaxError = msg => msg.includes('SyntaxError: invalid syntax');
-
-describe('POST python', () => {
+describe('POST ruby', () => {
   // beforeEach(done => done());
   let chaiServer;
   before(async () => {
@@ -25,18 +23,16 @@ describe('POST python', () => {
     chaiServer.post(path).end((err, res) => {
       expect(err).not.to.be.null;
       expect(res).to.have.status(400);
-      expect(isSyntaxError(res.text)).to.be.true;
+      expect(res.text.includes('syntax error')).to.be.true;
       done();
     });
   });
 
   it('should return anything that was printed', done => {
-    // identation is important in python!
     const payload = `
 x = 1
-while x < 5:
-    print(x)
-    x = x + 1
+puts x
+puts x + 1
       `;
     chaiServer
       .post(path)
@@ -45,42 +41,35 @@ while x < 5:
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
-        expect(JSON.parse(res.text)).to.deep.equal(['1', '2', '3', '4']);
+        expect(JSON.parse(res.text)).to.deep.equal(['1', '2']);
         done();
       });
   });
 
-  it('should report "not defined" errors', done => {
-    // ExecutionError:  foo is not defined
-    const payload = 'foo()';
-    const errorMessage = "NameError: name 'foo' is not defined";
+  it('should report various errors', done => {
     chaiServer
       .post(path)
       .set('content-type', 'text/plain')
-      .send(payload)
+      .send('foo()')
       .end((err, res) => {
         expect(err).not.to.be.null;
         // expect(res).to.have.status(400);
-        expect(res.text.includes(errorMessage)).to.be.true;
+        expect(res.text.includes('NoMethodError')).to.be.true;
         done();
       });
-  });
 
-  it('should report syntax errors', done => {
-    const payload = '^$%^';
-    const errorMessage = 'SyntaxError: invalid syntax';
     chaiServer
       .post(path)
       .set('content-type', 'text/plain')
-      .send(payload)
+      .send('^$%^')
       .end((err, res) => {
         expect(err).not.to.be.null;
         // expect(res).to.have.status(400);
         // console.log(res.text );
-        expect(res.text.includes(errorMessage)).to.be.true;
+        expect(res.text.includes('syntax error')).to.be.true;
         done();
       });
-  });
+    });
 
   // TODO: timebox script
 });
