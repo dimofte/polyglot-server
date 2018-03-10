9af07c567c19f89609e24a1b7d1e8b06e8b80ec1
Bogdan Dimofte <bogdan.dimofte+github@shop.co>
2018-03-10 11:21:56 +0200

    task manager: the implementation

---
+++ src/TaskManager.js
@@ -0,0 +1,35 @@
+const DEFAULT_LIMIT = 5;
+
+class TaskManager {
+  constructor(limit = DEFAULT_LIMIT) {
+    if (Number.isNaN(limit) || limit < 0) {
+      limit = DEFAULT_LIMIT;
+    }
+    this.limit = limit;
+    this.tasks = [];
+  }
+
+  queue() {
+    const { tasks, limit } = this;
+    const id = Math.random();
+    return new Promise(resolve => {
+      tasks.push({
+        id,
+        start: () => resolve(id)
+      });
+      if (tasks.length <= limit) {
+        resolve(id);
+      }
+    });
+  }
+
+  async markDone(id) {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+    const { tasks, limit } = this;
+    if (tasks.length === limit) {
+      tasks[limit - 1].start();
+    }
+  }
+}
+
+module.exports = TaskManager;
